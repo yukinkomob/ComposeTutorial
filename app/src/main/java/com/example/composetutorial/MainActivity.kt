5c9335e73c048203950bc5381c518d19143ca7c8
Yuki Nishimura <yukinkopen@gmail.com>
2022-12-25 01:47:15 +0900

    リストを表示 TODO: 項目をクリックした時にコンテンツを一行表示 or 全行表示で切り替える

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,8 +19,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.SampleData
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
+// content としてConversationを受け取り、SampleDataを渡す
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +60,10 @@ fun DefaultPreview() {
  * Imageにmodifierを追加し、sizeを40dp, clipをcircle shapeで設定
  * Imageの下に空間を8dp入れる
  * ColumnのTextとTextの間に空間を4dp入れる
+ * content description を null
+ * border を幅1.5dp, セカンダリ色指定、円形で設定
+ * タイポグラフィを追加
+ * isExpandedというStateを用意する
  */
 @Composable
 fun MessageCard(msg: Message) {
@@ -61,35 +71,89 @@ fun MessageCard(msg: Message) {
         Image(
             painter = painterResource(id = R.drawable.profile_picture),
             contentDescription = "Content Profile Picture",
-            modifier = Modifier.size(40.dp).clip(CircleShape)
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
+
+        // todo remember関数の値をisExpandedに保管する（再利用する）
+        // todo 値自体はMutableState<Boolean>型にfalseを渡す形
+
+        // todo Columnをclickする度にisExpandedの値が反転すること
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.message)
+            // todo Surfaceで囲ってやること
+            // todo     shapeにmediumを設定すること
+            // todo     elevationは1dpを設定すること
+            // todo Textは下記のように設定する
+                // text: message bodyを設定
+                // modifier: paddingを全体に4dp設定
+                // maxLines:
+                    // isExpanded:  Int.MAX_VALUE
+                    // !isExpanded: 1
+                // style: typography.body2を設定
+            Text(
+                text = msg.body,
+                style = MaterialTheme.typography.body2
+            )
         }
     }
 }
 
-data class Message(val author: String, val message: String)
+data class Message(val author: String, val body: String)
 
 /**
  * 関数をプレビューする
  * - Preview アノテーションを利用すること
  * - プレビュー可能なMessageCard関数を作成する
+ * - プロジェクト内で作成したテーマを指定する
+ * - Surfaceを使用する
+ * @note Light Modeの場合、デフォルトの値がそのまま使われる？
  */
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(Message("Android", "Jetpack Compose"))
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
 }
 
+// 関数名：Conversation
+// 引数：messages: List<Message>
+// LazyColumnを利用する
+//  messagesのitemをMessageCardに渡してそれをリストとして表示する
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        messages.map { item { MessageCard(it) } }
+    }
+}
+
+// 関数名：PreviewConversation
+// プレビューとして表示する
+// ComposeTutorialThemeを利用
+// Conversationを利用
+//   SampleDataを渡す
 @Preview
 @Composable
-fun MyImage() {
-    Image(
-        painter = painterResource(id = R.drawable.index_topc4),
-        contentDescription = "Content Profile Picture",
-    )
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
