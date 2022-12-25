699f5e2d949479299dcbcad7ba83fc84c3901598
Yuki Nishimura <yukinkopen@gmail.com>
2022-12-25 12:37:01 +0900

    importを削除

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -1,26 +1,7 @@
 package com.example.composetutorial
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.composetutorial.ui.SampleData
-import com.example.composetutorial.ui.theme.ComposeTutorialTheme
 
 // 利用しない
 class MainActivity : ComponentActivity() {
@@ -37,7 +18,7 @@ class MainActivity : ComponentActivity() {
  * ColumnのTextとTextの間に空間を4dp入れる
  * content description を null
  * border を幅1.5dp, セカンダリ色指定、円形で設定
- * タイポグラフィを追加
+ * タイポグラフィを追加
  * isExpandedというStateを用意する
  */
 
